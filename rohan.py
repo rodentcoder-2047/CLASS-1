d54a4579b28a250901306525b0f7ec4900db3fd4
rodentcoder-2047 <rodentcoder2047@gmail.com>
2025-11-01 15:47:22 +0530

    added name of favourite movie

--- rohan.py
+++ rohan.py
@@ -1,4 +1,16 @@
 print("hello world")
+#push 1
 name = " rohan sharma "
 print(name)
+#push 2
+age = 19
+print(age)
+#push 3
+def greet(name,age):
+    print(f"hello ,{name}")
+    print(f"you are {age}years old") 
 
+greet(name, age)
+#push 4
+favourite_movie= "YOU ARE NOT SUPPOSED TO TALK ABOUT IT "
+print(favourite_movie)
