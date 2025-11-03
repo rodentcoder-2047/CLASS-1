b6a9ccd9c6cbbeaac402d3260ccc5a9ecd230651
rodentcoder-2047 <rodentcoder2047@gmail.com>
2025-11-03 13:40:46 +0530

    college eligibility checker

---
+++ m.py
@@ -0,0 +1,7 @@
+print("College Eligibility Checker")
+cgpa = float(input("Enter your CGPA: "))
+
+if cgpa >= 7:
+    print("You are eligible for college")
+else:
+    print("You are not eligible")
---
+++ my schoo and eligibility for college.py
@@ -0,0 +1,20 @@
+print("Hello Rohan")
+hey = input(">")
+print(" came back from school")
+yes_it_is_tiring = input(">")
+print("It should be , your school Siddharth public school is situated on a long distance ")
+and_i__have_to_walk_on_foot = input(">")
+print("Walkling 3 km on foot daily is not a small task ")
+but_i_have__to__exams_are_over_and__i_have_to_study_for_second_attempt = input(">")
+print("Excited to go to college ")
+i_am_but_nervous_about_my_exam_result = input(">")
+print("That is one thing to worry about btw what results are you expecting")
+percentile = float(input("Enter your percentile:"))
+
+if percentile >=90.0 and percentile < 95.0:
+    print("Nice you are good to go  you just need the cuttoff marks to take admission ")
+elif percentile >=95.0:
+     print(" you will sure make it best colleges")   
+else:
+    print(" Oh no you need to work harder for the second attempt it is not enough to get the addmission ")
+
